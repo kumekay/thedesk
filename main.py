198dec0bfe345332268dc64008e4975c3c4e0c5f
Sergei Silnov <po@kumekay.com>
2021-09-22 23:08:57 +0200

    Add custom button implementation

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import gc
+import time
 
 import neopixel
 import uasyncio as asyncio
 from machine import PWM, Pin
-
-from button import PushButton
 
 gc.collect()
 
@@ -27,15 +26,57 @@ BUTTON_PINS = (
 PIXEL_PIN = Pin(33, Pin.OUT)
 PIXEL = neopixel.NeoPixel(PIXEL_PIN, 1)
 
-current_duty = 400
+current_duty = 1000
 
 
-def move(up=False):
+class Button:
+    def __init__(self, pin, on_press, on_release, debounce_ms=50, off_state=None):
+        self.pin = pin
+        self.on_press = on_press
+        self.on_release = on_release
+        self.debounce_ms = debounce_ms
+
+        if off_state is None:
+            off_state = pin.value()
+        self.off_state = off_state
+
+        self.state = self.raw_state()
+
+        asyncio.create_task(self.update_state())
+
+    def raw_state(self):
+        return bool(self.pin.value() ^ self.off_state)
+
+    async def update_state(self):
+        while True:
+            current_state = self.raw_state()
+
+            if self.state != current_state:
+                if current_state:
+                    asyncio.create_task(self.on_press())
+                else:
+                    asyncio.create_task(self.on_release())
+                self.state = current_state
+
+            await asyncio.sleep_ms(self.debounce_ms)
+
+
+async def move(up=False):
+    print("going {}".format(up))
     for i, pwm in enumerate(PWMS):
         pwm.duty((int(up) ^ i) * current_duty)
 
 
-def stop():
+async def down():
+    await move(up=False)
+
+
+async def up():
+    await move(up=True)
+
+
+async def stop():
+    print("stop")
     for pwm in PWMS:
         pwm.duty(0)
 
@@ -45,10 +86,8 @@ async def main():
     for pin in EN_PINS:
         pin.on()
 
-    for i, button_pin in enumerate(BUTTON_PINS):
-        button = PushButton(button_pin)
-        button.press_func(move, (i,))
-        button.release_func(stop)
+    Button(BUTTON_PINS[0], on_press=down, on_release=stop, off_state=True)
+    Button(BUTTON_PINS[1], on_press=up, on_release=stop, off_state=True)
 
     # Keep alive
     while True:
